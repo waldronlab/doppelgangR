dcf420af7c323d7443226d8793a4cf60783d1b8e
Levi Waldron <lwaldron.research@gmail.com>
2015-01-31 10:21:29 -0500

    Catch errors in TCGA.R

--- inst/TCGA.R
+++ inst/TCGA.R
@@ -11,7 +11,8 @@ tcga.res <- list()
 for (i in 1:length(all.datasets)){
     (ds.name <- all.datasets[i])
     if(!ds.name %in% names(tcga.res))
-        tcga.res[[ds.name]] <- getFirehoseData(ds.name, runDate=all.dates[1], RNAseq_Gene=TRUE, RNAseq2_Gene_Norm=TRUE, mRNA_Array=TRUE)
+        res <- try(getFirehoseData(ds.name, runDate=all.dates[1], RNAseq_Gene=TRUE, RNAseq2_Gene_Norm=TRUE, mRNA_Array=TRUE))
+        if(!is(res, "try-error")) tcga.res[[ds.name]] <- res
 }
 
 save(tcga.res, file="TCGA.rda")
